c7f5dce0ce13772ab21ae45f845e4993bb47e9ae
TheNumberGuy <tiagorocha1515@gmail.com>
2019-10-04 15:25:43 +0100

    Exercicio 1, 2, 3 parte 2 completos

--- src/Exercicio2/Exercicio2.java
+++ src/Exercicio2/Exercicio2.java
@@ -8,13 +8,14 @@ package Exercicio2;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import javafx.scene.effect.Light.Point;
 
 /**
  *
  * @author tiago
  */
 public class Exercicio2 {
-    
+
     static void printCollection(Collection<?> c) {
         for (Object e : c) {
             System.out.println(e);
@@ -54,19 +55,40 @@ public class Exercicio2 {
         System.out.println(teste5.equalElements());
         System.out.println(teste7.equalElements());
 
-        
         //Criação de uma Colection e adicao de elementos
         List<Integer> stones = new ArrayList<>();
-        
+
         stones.add(1);
         stones.add(2);
         stones.add(3);
         stones.add(4);
-        
+
         //teste printar elementos da collection
         printCollection(stones);
+
+        //Exercicio 5
+        //public void drawAll(List<? extends Shape>
         
+        //Parte 2
         
+        //Exercicio 1
+        //a) classe genérica
+        //b) classe / tipo parametrizado
+        //d) genérica
+        
+        //Exercicio 2
+        //da
+        //nao da
+        //nao da
+        
+        //Exercicio 3
+        /*
+        ArrayList<Point> a = new ArrayList<Point>();
+        ArrayList<Object> b;
+        b = a;
+        b.add(new Point(10, 20));
+        */
+        //Os tipos não são compativeis
     }
 
 }
