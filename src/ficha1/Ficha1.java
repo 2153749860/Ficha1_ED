e6d562e8360cda4328034d25ec5eceab00175242
TheNumberGuy <tiagorocha1515@gmail.com>
2019-10-04 13:40:50 +0100

    Exercicio 1 completo

--- src/ficha1/Ficha1.java
+++ src/ficha1/Ficha1.java
@@ -16,6 +16,19 @@ public class Ficha1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        TwoTypePair<String, Integer> teste1 = new TwoTypePair<>("Tiago", 20);
+        TwoTypePair<String, Integer> teste2 = new TwoTypePair<>("Tiago", 20);
+        TwoTypePair<String, Integer> teste3 = new TwoTypePair<>("Vitor", 20);
+        
+        //Teste printar objetos de teste1
+        System.out.println(teste1.getFirst());
+        System.out.println(teste1.getSecond());
+        
+        //teste comparar se objetos sao iguais
+        System.out.println(teste1.equals(teste2));
+        
+        //teste comparar se objetos nao sao iguais
+        System.out.println(teste1.equals(teste3));
     }
     
 }
---
+++ src/ficha1/TwoTypePair.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ficha1;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author tiago
+ */
+public class TwoTypePair<T1, T2> {
+    private T1 first;
+    private T2 second;
+
+    public TwoTypePair(T1 first, T2 second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public TwoTypePair() {
+        first = null;
+        second = null;
+    }
+
+    public T1 getFirst() {
+        return first;
+    }
+
+    public void setFirst(T1 first) {
+        this.first = first;
+    }
+
+    public T2 getSecond() {
+        return second;
+    }
+
+    public void setSecond(T2 second) {
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return ("First: " + first.toString() + "\n" 
+                + "second: " + second.toString());
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object otherObject) {
+        if(otherObject == null) {
+           return false;
+        }
+        else{
+            if(getClass() != otherObject.getClass()){
+                return false;
+            }
+            else{
+                TwoTypePair otherPair = (TwoTypePair) otherObject;
+                return (first.equals(otherPair.first) 
+                        && second.equals(otherPair.second));
+            }
+        }
+    }
+    
+    
+    
+    
+}
